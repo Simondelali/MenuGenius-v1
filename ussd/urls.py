cfb902487c5c7530400a0720488f728af4da53c2
Simondelali <atiegardelali@gmail.com>
2024-05-04 20:18:10 +0000

    implemented sub-options(3 levels)

--- ussd/urls.py
+++ ussd/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import USSDSession, ussd_endpoint, process_input
 
 urlpatterns = [
-    path('', ussd_endpoint, name = 'endpoint')
+    path('', ussd_endpoint, name = 'endpoint'),
 ]
--- ussd/views.py
+++ ussd/views.py
@@ -13,28 +13,27 @@ class USSDSession:
         self.user_data = {}
 
 def ussd_endpoint(request):
-    # Create or retrieve USSD session object for the user
-    session_id = request.GET.get('session_id')
-    if session_id in active_sessions:
-        session = active_sessions[session_id]
-    else:
-        session = USSDSession()
-        active_sessions[session_id] = session
+    if request.method == 'GET':
+        # Create or retrieve USSD session object for the user
+        session_id = request.GET.get('session_id')
+        if session_id in active_sessions:
+            session = active_sessions[session_id]
+        else:
+            session = USSDSession()
+            active_sessions[session_id] = session
 
-    # Parse incoming USSD request
-    ussd_input = request.GET.get('text', '')
+        # Parse incoming USSD request
+        ussd_input = request.GET.get('text', '')
 
-    # Process USSD request based on current state
-    response = process_input(session, ussd_input) 
+        # Process USSD request based on current state
+        response = process_input(session, ussd_input) 
 
-    # Send USSD response
-    return HttpResponse(response)
+        # Send USSD response
+        return HttpResponse(response)
 
 def process_input(session, input):
-    # Define the logic to process USSD input based on the current state
     if session.state == 'start':
         response = "CON Welcome to Sample USSD App. Select a menu:\n"
-        # Fetch menus from the database
         menus = Menu.objects.all()
         for i, menu in enumerate(menus, start=1):
             response += f"{i}. {menu.name}\n"
@@ -44,7 +43,7 @@ def process_input(session, input):
             selected_menu_index = int(input)
             selected_menu = Menu.objects.all()[selected_menu_index - 1]
             session.selected_menu = selected_menu
-            options = selected_menu.menuoption_set.all()
+            options = selected_menu.menuoption_set.filter(parent_option=None)  # Only top-level options
             response = "CON Choose an option:\n"
             for i, option in enumerate(options, start=1):
                 response += f"{i}. {option.name}\n"
@@ -54,10 +53,26 @@ def process_input(session, input):
     elif session.state == 'select_option':
         try:
             selected_option_index = int(input)
-            selected_option = session.selected_menu.menuoption_set.all()[selected_option_index - 1]
-            response = f"END You selected: {selected_option.name}. Value: {selected_option.value}"
-            session.state = 'start'
+            selected_option = session.selected_menu.menuoption_set.filter(parent_option=None)[selected_option_index - 1]
+            sub_options = selected_option.child_options.all()
+            if sub_options.exists():
+                response = "CON Choose a sub-option:\n"
+                for i, sub_option in enumerate(sub_options, start=1):
+                    response += f"{i}. {sub_option.name}\n"
+                session.state = 'select_sub_option'
+            else:
+                response = f"END You selected: {selected_option.name}. Value: {selected_option.value}"
+                session.state = 'start'
         except (ValueError, IndexError):
             response = "END Invalid input. Please select a valid option."
+    elif session.state == 'select_sub_option':
+        try:
+            selected_sub_option_index = int(input)
+            selected_sub_option = session.selected_menu.menuoption_set.filter(parent_option=None)[selected_sub_option_index - 1]
+            response = f"END You selected: {selected_sub_option.name}. Value: {selected_sub_option.value}"
+            session.state = 'start'
+        except (ValueError, IndexError):
+            response = "END Invalid input. Please select a valid sub-option."
 
     return response
+
